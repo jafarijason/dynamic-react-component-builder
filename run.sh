bfebf1c428bcfa0dad8cf65f72cd25af3d68909f
Jason Jafari <me@jasonjafari.com>
2024-07-30 00:04:55 -0400

    WIP

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ export DRCB_DATA_DIR="$HOME/.drcb_data_dir"
 mkdir -p $DRCB_DATA_DIR
 
 # Get the directory containing the script
-DRCB_MODULE_DIR=$(dirname "$0")
+DRCB_MODULE_DIR=$(dirname "$(realpath "$0")")
 
-# Convert the directory to an absolute path
-DRCB_MODULE_DIR=$(cd "$DRCB_MODULE_DIR" && pwd)
+# # Convert the directory to an absolute path
+# DRCB_MODULE_DIR=$(cd "$DRCB_MODULE_DIR" && pwd)
 
-# echo "The script is located in: $DRCB_MODULE_DIR"
+echo "The script is located in: $DRCB_MODULE_DIR"
 
 
 
