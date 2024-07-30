a34e144309c56aace9075195c821f3adf8228d8e
Jason Jafari <me@jasonjafari.com>
2024-07-29 23:55:27 -0400

    WIP

--- run.sh
+++ run.sh
@@ -8,6 +8,14 @@ export DRCB_DATA_DIR="$HOME/.drcb_data_dir"
 echo "DRCB_DATA_DIR $DRCB_DATA_DIR"
 mkdir -p $DRCB_DATA_DIR
 
+# Get the directory containing the script
+SCRIPT_DIR=$(dirname "$0")
+
+# Convert the directory to an absolute path
+SCRIPT_DIR=$(cd "$SCRIPT_DIR" && pwd)
+
+echo "The script is located in: $SCRIPT_DIR"
+
 
 
 bun index.ts
