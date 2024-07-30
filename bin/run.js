6ac5e6b2c033fdba49c510098630732a96c89328
Jason Jafari <me@jasonjafari.com>
2024-07-29 23:32:27 -0400

    WIP

--- bin/run.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bun
-
-// // eslint-disable-next-line unicorn/prefer-top-level-await
-// (async () => {
-//   const oclif = await import('@oclif/core')
-//   await oclif.execute({dir: __dirname})
-// })()
-
-console.log('drcb')
---
+++ run.sh
@@ -0,0 +1,4 @@
+export DRCB_HOME_DIR="~/.drcb_home_dir"
+mkdir -p $DRCB_HOME_DIR
+
+bun index.ts
